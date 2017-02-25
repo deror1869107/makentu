d43cb462fbb615df693d675baa76c6f3f836dc4e
deror1869107 <sungyuanyao@gmail.com>
2017-02-25 17:44:32 +0000

    Fix homepage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,6 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     @garage = @event.garage
     @event.destroy
-    redirect_to garages_path
+    redirect_to garage_path(@garage)
   end
 end
--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,11 +1,8 @@
 class GaragesController < ApplicationController
   before_filter :authenticate_user!
 
-  def index
+  def show
     @garage = current_user.garage
     @events = @garage.events 
-  end
-
-  def show
   end
 end
